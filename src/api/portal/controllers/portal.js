f105230f265be40c71ba87ec3bc54f092134b336
Felipe Oliveira Lourenço <liloo.plataform@gmail.com>
2023-03-06 11:14:18 -0300

    Inicial

---
+++ src/api/portal/controllers/portal.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * portal controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::portal.portal');
---
+++ src/api/portal/routes/portal.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * portal router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::portal.portal');
